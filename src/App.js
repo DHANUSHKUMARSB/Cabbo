a4cef72c49ddfbd6900cb8c4965dd2aec80a0a00
DHANUSHKUMARSB <dhanushkumar9342@gmail.com>
2024-07-04 15:46:56 +0530

    finished responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
+import { useState } from "react";
+
 function App() {
   // start code here
-  return <div className=""></div>;
+  return (
+    <div className="hero">
+      <Nav />
+    </div>
+  );
 }
+function Menu({ isOpen }) {
+  const menu = [
+    "Home",
+    "About Us",
+    "Safety",
+    "Careers",
+    "Community",
+    "Contact Us",
+  ];
 
+  return (
+    <div className={`menu-content ${isOpen ? "active" : ""}`}>
+      {menu.map((data, index) => (
+        <span className="menu-item" key={index}>
+          {data}
+        </span>
+      ))}
+    </div>
+  );
+}
+function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="nav">
+      <img className="logo" src="Images/Logo.png" alt="logo" />
+      <div className="menu">
+        <img
+          className="ham"
+          src="Images/Menu.png"
+          alt="ham"
+          onClick={toggleMenu}
+        />
+        <Menu isOpen={isOpen} />
+      </div>
+    </div>
+  );
+}
 export default App;
